deb1c585833f57b70d787cff858465cfd8bc5caa
Nathan Hernandez <email@nathanph.com>
2015-02-25 08:05:20 +0000

    Reads lines from ush shell.

--- ush.c
+++ ush.c
@@ -1,21 +1,55 @@
+#include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include "ush.h"
 
 int main( int argc, const char* argv[] ) {
+    char buffer[MAX_STRING_SIZE];
+    char commands[MAX_COMMANDS][MAX_STRING_SIZE];
 
-    char buffer[100];
-
-    displayPrompt();
-    getCommand( buffer, size );
-
+    while (true) {
+        displayPrompt();
+        getCommand( buffer );
+        parseCommand( buffer, commands );
+    
+//        for(int i=0;i<commandCount; i++)
+//        {
+//            printf("%s\n", commands[i]);
+//        }
+    
+    }
 }
 
-void getCommand(char *buffer, size_t size) {
-    fgets(buff, size, stdin);
+void getCommand(char *buffer) {
+    fgets(buffer, MAX_STRING_SIZE, stdin);
+
+    // Remove newline character
+    for (int i=0; i<MAX_STRING_SIZE; i++)
+    {
+        if(buffer[i] == '\n')
+        {
+            buffer[i] = '\0';
+        }
+    }
 }
 
 void displayPrompt() {
     printf("ush> ");
+}
+
+int parseCommand(char *buffer, char commands[MAX_COMMANDS][MAX_STRING_SIZE]) {
+    char *token;
+    int commandCount=0;
+
+    token = strtok(buffer, "|");
+
+    while (token != NULL)
+    {
+        strcpy(commands[commandCount++], token);
+        token = strtok(NULL, "|");
+    }
+
+    return commandCount;
 }
 
 
--- ush.h
+++ ush.h
@@ -1,8 +1,14 @@
 #ifndef USH_H
 #define USH_H
 
-void getCommand();
+#define MAX_STRING_SIZE 100
+#define MAX_COMMANDS    10
+
+#include <string.h>
+
+void getCommand(char *buffer);
 void displayPrompt();
+int parseCommand(char *buffer, char commands[MAX_COMMANDS][MAX_STRING_SIZE]);
 
 #endif /* USH_H */
 
